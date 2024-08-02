b53d6f12271f5a718dab9828616ec9b20e182a06
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-08-02 13:19:14 +0200

    Added 2nd recommentation

--- src/components/recommendations/recommendation.component.jsx
+++ src/components/recommendations/recommendation.component.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import PdfViewer from "../../common/PdfViewer";
 
-import pdf from "../../Assets/Aviwe-DevDream-Recommendation.pdf";
+import devDFreamPdf from "../../Assets/Aviwe-DevDream-Recommendation.pdf";
+import LettuceDigitalPdf from "../../Assets/Recommendations _ Aviwe_Ngqukumba _ LinkedIn.pdf";
 
 function Recommendations() {
-  return <PdfViewer pdf={pdf} name="DevDream R.." />;
+  return (
+    <Fragment>
+      <PdfViewer pdf={devDFreamPdf} name="DevDream R.." />;
+      <PdfViewer pdf={LettuceDigitalPdf} name="Lettuce R.." />;
+    </Fragment>
+  );
 }
 
 export default Recommendations;
